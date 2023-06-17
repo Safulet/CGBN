0f175298b7714058b495c41729ab90fda7f1db1e
ChunKunWang <wckamos@gmail.com>
2023-06-17 08:41:52 +0000

    impl of powm with fixed/sliding window

--- shared_lib/main.go
+++ shared_lib/main.go
@@ -1,20 +1,22 @@
 package main
 
 /*
-   #cgo LDFLAGS: -L. -L./ -lcalc
-   void cgbn_power(int size);
-   void cgbn_power_uma(int size);
-   void cgbn_power_zc(int size);
-   void omp_gmp_power(int size);
-   void gmp_power(int size);
+#cgo LDFLAGS: -L. -L./ -lcalc
+void cgbn_power(int size);
+void cgbn_power_uma(int size);
+void cgbn_power_zc(int size);
+void powm_fixed_window(int size);
+void powm_sliding_window(int size);
+void omp_gmp_power(int size);
+void gmp_power(int size);
 */
 import "C"
 
 import (
 	"fmt"
 	"time"
-  "os"
-  "strconv"
+	"os"
+	"strconv"
 )
 
 func run_cuda(size int) {
@@ -29,6 +31,14 @@ func run_cuda_zc(size int) {
 	C.cgbn_power_zc(C.int(size))
 }
 
+func run_powm_fixed_window(size int) {
+	C.powm_fixed_window(C.int(size))
+}
+
+func run_powm_sliding_window(size int) {
+	C.powm_sliding_window(C.int(size))
+}
+
 func run_omp_gmp(size int) {
 	C.omp_gmp_power(C.int(size))
 }
@@ -38,10 +48,10 @@ func run_gmp(size int) {
 }
 
 func main() {
-  size, _ := strconv.Atoi(os.Args[1])
+	size, _ := strconv.Atoi(os.Args[1])
 
-  //size := 5
-  num := 10
+	//size := 5
+	num := 10
 
 	for i := 0; i < num; i++ {
 		startTime := time.Now()
@@ -49,30 +59,44 @@ func main() {
 		fmt.Println(fmt.Sprintf("run_cuda cost %v", time.Now().Sub(startTime)))
 	}
 
-  fmt.Println(fmt.Sprintf("========================="))
+	fmt.Println(fmt.Sprintf("========================="))
 	for i := 0; i < num; i++ {
 		startTime := time.Now()
 		run_cuda_uma(size)
 		fmt.Println(fmt.Sprintf("run_cuda_uma cost %v", time.Now().Sub(startTime)))
 	}
 
-  fmt.Println(fmt.Sprintf("========================="))
+	fmt.Println(fmt.Sprintf("========================="))
 	for i := 0; i < num; i++ {
 		startTime := time.Now()
 		run_cuda_zc(size)
 		fmt.Println(fmt.Sprintf("run_cuda_zc cost %v", time.Now().Sub(startTime)))
 	}
 
-/*
-  fmt.Println(fmt.Sprintf("========================="))
+	/*
+	fmt.Println(fmt.Sprintf("========================="))
 	for i := 0; i < num; i++ {
 		startTime := time.Now()
 		run_omp_gmp(size)
 		fmt.Println(fmt.Sprintf("run_omp_gmp cost %v", time.Now().Sub(startTime)))
 	}
-  */
+	*/
 
-  fmt.Println(fmt.Sprintf("========================="))
+	fmt.Println(fmt.Sprintf("========================="))
+	for i := 0; i < num; i++ {
+		startTime := time.Now()
+		run_powm_fixed_window(size)
+		fmt.Println(fmt.Sprintf("run_powm_fixed_window cost %v", time.Now().Sub(startTime)))
+	}
+
+	fmt.Println(fmt.Sprintf("========================="))
+	for i := 0; i < num; i++ {
+		startTime := time.Now()
+		run_powm_sliding_window(size)
+		fmt.Println(fmt.Sprintf("run_powm_sliding_window cost %v", time.Now().Sub(startTime)))
+	}
+
+	fmt.Println(fmt.Sprintf("========================="))
 	for i := 0; i < num; i++ {
 		startTime := time.Now()
 		run_gmp(size)
